ecb0ebb0beb3889bdcfd49bad2993939c63da156
why-project96 <whyproject00@gmail.com>
2022-08-30 00:22:05 +0700

    main

---
+++ ssh/delssh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# My Telegram : https://t.me/whyvpn1
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/why-project96/perizinan/main/ipvps.txt | grep $MYIP )
+if [ $MYIP = $MYIP ]; then
+echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+else
+echo -e "${NC}${RED}Permission Denied!${NC}";
+echo -e "${NC}${LIGHT}Please Contact Admin!!"
+echo -e "${NC}${LIGHT}Facebook : NO HAVE"
+echo -e "${NC}${LIGHT}WhatsApp : 081212712087"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/whyvpn1"
+exit 0
+fi
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "Username $Pengguna Telah Di Hapus"
+else
+        echo -e "Failure: Username $Pengguna Tidak Ada"
+fi
